83e7225589df60d6e61c497541ff8aab844fccc6
Martin Algesten <martin@algesten.se>
2021-07-23 10:48:34 +0200

    gen: sometimes duplicate randomp patterns

--- src/gen.rs
+++ src/gen.rs
@@ -6,7 +6,7 @@ const DEFAULT_PATTERN_LEN: u8 = 64;
 const DEFAULT_TRACK_LEN: u8 = 64;
 
 /// Base for seed since starting at 0 is so boring.
-pub const SEED_BASE: i32 = 0x616c67;
+pub const SEED_BASE: i32 = 0x4164c47;
 
 pub const STOKAST_PARAMS: Params<4> = Params {
     seed: SEED_BASE as u32,
@@ -120,7 +120,7 @@ impl<const X: usize> Generated<X> {
                 params: params.tracks[i],
             };
 
-            *pat = gen.generate(params.pattern_length as usize);
+            *pat = generate(gen.seed, &gen.params, params.pattern_length as usize);
         }
 
         // reserve 64 track specific rnd before letting it go.
@@ -140,65 +140,102 @@ impl<const X: usize> Generated<X> {
     }
 }
 
-impl TrackGenerator {
-    fn generate(&self, pattern_length: usize) -> Pattern {
-        let mut rnd = Rnd::new(self.seed);
+fn generate(seed: u32, params: &TrackParams, pattern_length: usize) -> Pattern {
+    let mut rnd = Rnd::new(seed);
 
-        if self.params.length == 0 {
-            // disabled
-            return Pattern::new_with(0, pattern_length);
-        }
-
-        // Ensure length is at least the number of steps.
-        let length = self.params.length.max(self.params.steps);
-
-        // This tries to find a subdivision of the pattern length. We use that as range
-        // for randomizing the euclidean steps when the user parameter is set to 0 (random).
-        // This is because I don't think  a large number of steps doesn't make for very
-        // interesting rhythms (almost every 16th sounding with some euclidean distributed "holes").
-        let range = {
-            const DIV: &[u32] = &[61, 53, 41, 31, 23, 16, 15, 14, 13, 11, 8, 7, 6, 5, 4];
-
-            let mut i = 0;
-
-            loop {
-                if length < 4 || i == DIV.len() {
-                    break pattern_length as u32;
-                }
-
-                if length as u32 / DIV[i] > 0 {
-                    break DIV[i];
-                }
-
-                i += 1;
-            }
-        };
-
-        // important to generate this also when it's not used since we need rnd.next() every time.
-        let random_steps = {
-            let unweighted = rnd.next() / (u32::max_value() / range);
-
-            let weighted = (unweighted * self.params.density as u32) / 127;
-
-            (weighted as u8) + 1
-        };
-
-        let steps = if self.params.steps == 0 {
-            random_steps as u8
-        } else {
-            self.params.steps
-        };
-
-        euclid(steps, length)
-            .offset(self.params.offset)
-            .repeat_to(pattern_length)
+    if params.length == 0 {
+        // disabled
+        return Pattern::new_with(0, pattern_length);
     }
+
+    // Ensure length is at least the number of steps.
+    let length = params.length.max(params.steps);
+
+    // This tries to find a subdivision of the pattern length. We use that as range
+    // for randomizing the euclidean steps when the user parameter is set to 0 (random).
+    // This is because I don't think a large number of steps makes for very interesting
+    // rhythms (almost every 16th sounding with some euclidean distributed "holes").
+    let range = {
+        const DIV: &[u32] = &[
+            61, 53, 41, 32, 31, 24, 23, 16, 15, 14, 13, 11, 8, 7, 6, 5, 4,
+        ];
+
+        let mut i = 0;
+
+        loop {
+            if length < 4 || i == DIV.len() {
+                break pattern_length as u32;
+            }
+
+            if length as u32 % DIV[i] == 0 {
+                break DIV[i];
+            }
+
+            i += 1;
+        }
+    };
+
+    {
+        // If the range is possible to subdivide by 4 or 2 to get something in the range of 6-8,
+        // we _sometimes_ do that to create variation.
+        const SUBDIVIDE: &[u32] = &[4, 2];
+
+        let x = rnd.next();
+
+        for sub in SUBDIVIDE {
+            if range % *sub == 0 {
+                // 33% of the time, if we are in random mode.
+                if params.steps == 0 && x < u32::MAX / 3 {
+                    let len = range / *sub;
+                    if len == 8 || len == 6 {
+                        // let's do it!
+                        let mut new_params = TrackParams {
+                            length: len as u8,
+                            offset: 0,
+                            ..*params
+                        };
+
+                        let p1 = generate(x, &new_params, len as usize);
+
+                        new_params.density = params.density * 2;
+                        let p2 = generate(x + 1, &new_params, len as usize);
+
+                        // This is the whole point.
+                        let combined = p1 + p2;
+
+                        return combined.offset(params.offset).repeat_to(pattern_length);
+                    }
+                }
+            }
+        }
+    }
+
+    // important to generate this also when it's not used since we need rnd.next() every time.
+    let random_steps = {
+        let unweighted = rnd.next() / (u32::max_value() / range);
+
+        let weighted = if params.density == 0 {
+            unweighted
+        } else {
+            (unweighted * params.density as u32) / 127
+        };
+
+        (weighted as u8) + 1
+    };
+
+    let steps = if params.steps == 0 {
+        random_steps as u8
+    } else {
+        params.steps
+    };
+
+    euclid(steps, length)
+        .offset(params.offset)
+        .repeat_to(pattern_length)
 }
 
 #[cfg(test)]
 mod test {
-    use std::time::SystemTime;
-
     use super::*;
     use crate::drums::Drums;
 
@@ -207,17 +244,14 @@ mod test {
         if true {
             return;
         }
-        let x = SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .unwrap()
-            .as_secs_f32() as u32;
+        let x = STOKAST_PARAMS.seed;
         for i in x..(x + 1000) {
             let mut drums = Drums::new();
 
             let mut params = STOKAST_PARAMS;
             params.seed = i;
 
-            let g: Generated<4> = Generated::new(STOKAST_PARAMS);
+            let g: Generated<4> = Generated::new(params);
 
             for i in 0..g.len() {
                 drums.add_pattern(g.patterns[i]);
